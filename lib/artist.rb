93717da838a70dbdeec54cdca35a8ae183015a93
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-27 03:24:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,23 @@ class Artist
     @@all
   end
 
+  def add_song(song)
+    song.artist = self
+  end
 
+  def songs
+    Song.all.select{|song| song.artist == self}
+  end
+
+  def self.find(name)
+    self.all.find{|artist| artist.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  
 
 
 
