37a277d79ad316b895fc802f00fdeffa803cacba
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-27 02:37:28 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -5,4 +5,11 @@ class MP3Importer
     @path = path
   end
 
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect {|file| file.gsub("#{path}/", "") }
+  end
+
+  def import
+    files.each{|file| Song.new_by_filename(file)}
+  end
 end
