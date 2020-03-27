078106db9e054276b3e27fe75a71ca051d6ea529
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-03-27 02:13:16 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,13 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs 
   @@all = []
 
   def initialize(name)
     @name = name
-    @songs = []
+    save
+  end
+
+  def save
     @@all << self
   end
 
